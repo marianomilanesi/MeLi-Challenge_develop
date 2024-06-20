ed374d0d201fde7ad4e95a3ddacb0580540680b5
Mariano <milanesimariano@gmail.com>
2024-06-20 20:55:43 -0300

    se subio la parte del codigo que ya funcionaba

--- enviar_mail.py
+++ enviar_mail.py
@@ -1,3 +1,5 @@
+# enviar_mail.py
+
 import sqlite3
 import smtplib
 from email.mime.text import MIMEText
@@ -5,8 +7,8 @@ from email.mime.multipart import MIMEMultipart
 
 SMTP_SERVER = 'smtp.office365.com'
 SMTP_PORT = 587
-SMTP_USER = 'intru505@hotmail.com'
-SMTP_PASSWORD = 'Primavera96!'
+SMTP_USER = 'tu_correo'  # Aquí debes colocar tu dirección de correo
+SMTP_PASSWORD = 'tu_password'  # Aquí debes colocar tu contraseña
 
 def send_email(to_email, subject, body):
     msg = MIMEMultipart()
@@ -26,19 +28,24 @@ def send_email(to_email, subject, body):
         print(f"Error al enviar el email: {e}")
 
 def enviar_emails():
-    conn = sqlite3.connect('databases_classification.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-    SELECT owner_email, manager_email, database_name
-    FROM database_info
-    WHERE classification = 'high'
-    ''')
-    high_classification_databases = cursor.fetchall()
-    
-    for db in high_classification_databases:
-        owner_email, manager_email, database_name = db
-        subject = f"Revalidación de Clasificación de {database_name}"
-        body = f"Estimado/a,\n\nPor favor, confirme la clasificación 'high' de la base de datos {database_name} propiedad de {owner_email}.\n\nSaludos,\nEquipo de Seguridad Informática"
-        send_email(manager_email, subject, body)
-    
-    conn.close()
+    try:
+        conn = sqlite3.connect('databases_classification.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+        SELECT owner_email, manager_email, database_name
+        FROM database_info
+        WHERE classification = 'high'
+        ''')
+        high_classification_databases = cursor.fetchall()
+        
+        for db in high_classification_databases:
+            owner_email, manager_email, database_name = db
+            subject = f"Revalidación de Clasificación de {database_name}"
+            body = f"Estimado/a,\n\nPor favor, confirme la clasificación 'high' de la base de datos {database_name} propiedad de {owner_email}.\n\nSaludos,\nEquipo de Seguridad Informática"
+            send_email(manager_email, subject, body)
+        
+    except sqlite3.Error as e:
+        print(f"Error al consultar la base de datos: {e}")
+    finally:
+        if conn:
+            conn.close()
